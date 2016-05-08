5decebaf5f94362966b73ce17ef2a802a902bae5
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-05-08 16:40:49 +0100

    Improved name of Member class. Fixed Slick bindings.

--- app/models/CaseClassMappings.scala
+++ app/models/CaseClassMappings.scala
@@ -3,17 +3,22 @@ package models
 import java.util
 
 import play.api.data.validation.Valid
-import slick.ast.ColumnOption.{AutoInc, PrimaryKey}
 
-object Tables extends {
-  val profile = slick.driver.PostgresDriver
-} with Tables
+import slick.driver.PostgresDriver.api._
 
-/** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
-trait Tables {
-  val profile: slick.driver.JdbcProfile
-  import profile.api._
+/**
+  * The case to SQL mappings for Slick.
+  */
+object CaseClassMappings extends App {
 
+  /**
+    * The main member class, representing a person that has provided us with a phone number or e-mail address.
+    *
+    * @param name Peron's (first) name
+    * @param phoneNumber UK mobile phone number
+    * @param email E-mail address
+    * @param id Record ID, if saved in DB
+    */
   case class Member(name: String, phoneNumber: String, email: String, id: Option[Int] = None) {
     /**
       * Ensures that the record has at least:
@@ -46,13 +51,21 @@ trait Tables {
     }
   }
 
+  /**
+    * The Slick mapping for Member instances.
+    *
+    * @param tag
+    */
   class Members(tag: Tag) extends Table[Member](tag, "members") {
-    def id = column[Int]("id", PrimaryKey, AutoInc)
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
     def name = column[String]("name")
     def phoneNumber = column[String]("phone_number")
     def email = column[String]("email")
     def * = (name, phoneNumber, email, id.?) <> (Member.tupled, Member.unapply)
   }
 
+  /**
+    * The object used to perform queries on Members in the database.
+    */
   val members = TableQuery[Members]
 }
