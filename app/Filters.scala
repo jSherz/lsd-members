87df50fb3dccb6272b96ead0402432f7a082e367
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-05-15 10:47:56 +0100

    Removed the example filter - not used.

--- app/Filters.scala
+++ app/Filters.scala
@@ -23,8 +23,6 @@
   */
 
 import javax.inject._
-
-import filters.ExampleFilter
 import play.api._
 import play.api.http.HttpFilters
 
@@ -38,19 +36,17 @@ import play.api.http.HttpFilters
  * the `application.conf` configuration file.
  *
  * @param env Basic environment settings for the current application.
- * @param exampleFilter A demonstration filter that adds a header to
  * each response.
  */
 @Singleton
-class Filters @Inject() (
-  env: Environment,
-  exampleFilter: ExampleFilter) extends HttpFilters {
+class Filters @Inject() (env: Environment) extends HttpFilters {
 
   override val filters = {
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
-    if (env.mode == Mode.Dev) Seq(exampleFilter) else Seq.empty
+    // if (env.mode == Mode.Dev) Seq(filterA, filterB) else Seq.empty
+    Seq.empty
   }
 
 }
