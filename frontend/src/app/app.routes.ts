7cfa1115ec5c5a7d3e9212db67d8311e251656f6
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-06-27 23:33:41 +0100

    Got the signup form to be displayed with Angular.

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { provideRouter, RouterConfig } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+
+export const routes: RouterConfig = [
+  { path: '', component: SignupComponent }
+];
+
+export const APP_ROUTER_PROVIDERS = [
+  provideRouter(routes)
+];
---
+++ frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'signup.component.html'
+})
+export class SignupComponent { }
