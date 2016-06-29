5153e171dbfed49f06b906b3c049e298b076fd9f
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-06-29 17:06:41 +0100

    Wired up the month service.

--- frontend/src/app/admin/courses/course-calendar.component.ts
+++ frontend/src/app/admin/courses/course-calendar.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment           from 'moment';
+import { MonthService }      from './month.service';
 
 @Component({
   moduleId: module.id,
-  templateUrl: 'course-calendar.component.html'
+  templateUrl: 'course-calendar.component.html',
+  providers: [MonthService]
 })
 export class CourseCalendarComponent implements OnInit {
 
   private startMonth = moment().subtract(1, 'years');
 
-  constructor() {}
+  constructor(private monthService: MonthService) {}
 
-  months: moment.Moment[] = [...Array(36)].map((_, i) =>
-    this.startMonth.add(i + 1, 'months'));
+  months = this.monthService.get(moment());
 
   ngOnInit() { }
 
