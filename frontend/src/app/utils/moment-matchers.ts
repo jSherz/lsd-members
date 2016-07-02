16605d4bb7b4a914b32001cfe43fd070e8929c22
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-07-02 14:04:13 +0100

    Started work on a custom moment matcher.

---
+++ frontend/src/app/utils/moment-matchers.ts
@@ -0,0 +1,21 @@
+import * as moment from 'moment';
+
+export const MOMENT_MATCHERS = {
+  toBeSame: (util, customEqualityTesters) => {
+    return {
+      compare: (actual: moment.Moment, expected: moment.Moment) => {
+        let result: any = { };
+
+        result.pass = actual.isSame(expected);
+
+        if (result.pass) {
+          result.message = `${expected} was the same as ${actual}`;
+        } else {
+          result.message `${expected} was not the same as ${actual}`;
+        }
+
+        return result;
+      }
+    };
+  }
+};
