db488ded8403c7e722f8dbbc106d54c6f310a0cd
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-07-02 17:51:57 +0100

    Added specs for the tile component.

--- frontend/src/app/admin/courses/tile.component.spec.ts
+++ frontend/src/app/admin/courses/tile.component.spec.ts
@@ -10,11 +10,36 @@ import {
   async, inject
 } from '@angular/core/testing';
 
+import * as moment       from 'moment';
+import { Tile }          from './tile.service';
 import { TileComponent } from './tile.component';
 
 describe('Component: Tile', () => {
-  it('should create an instance', () => {
+
+  let mock = (tile: Tile) => {
     let component = new TileComponent();
-    expect(component).toBeTruthy();
+    component.tile = tile;
+
+    return component;
+  }
+
+  it('selects the correct class for today\'s tile', () => {
+    let comp = mock(new Tile(moment(), false, false, true));
+
+    expect(comp.otherClasses()).toEqual('tile-today');
+  });
+
+  it('selects the correct class for a day in the previous or next months', () => {
+    let compPrevMonth = mock(new Tile(moment(), true, false, false));
+    let compNextMonth = mock(new Tile(moment(), false, true, false));
+
+    expect(compPrevMonth.otherClasses()).toEqual('tile-other-month');
+    expect(compNextMonth.otherClasses()).toEqual('tile-other-month');
+  });
+
+  it('does not have any additional class if not today and this month', () => {
+    let comp = mock(new Tile(moment(), false, false, false));
+
+    expect(comp.otherClasses()).toEqual('');
   });
 });
