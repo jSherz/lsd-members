d0c8b4e792ef27dde3660fb1aa736eadaf20fb09
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-08-03 20:37:07 +0100

    Added files missing from last commit.

---
+++ frontend/e2e/signup.service.stub.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { ISignupService, SignupResult } from '../src/app/signup';
+import '../src/app/rxjs-operators';
+
+@Injectable()
+export class SignupServiceStub implements ISignupService {
+
+    static validName: string = "Abby North";
+    static validEmail: string = "AbbyNorth@teleworm.us";
+    static validPhoneNumber: string = "07058074719";
+
+    static inUseName: string = "Mohammed Charlton";
+    static inUseEmail: string = "MohammedCharlton@jourrapide.com";
+    static inUsePhoneNumber: string = "07985457231";
+
+    static apiFailName: string = "Amy Farrell";
+    static apiFailEmail: string = "AmyFarrell@armyspy.com";
+    static apiFailPhoneNumber: string = "07827651140";
+
+    signup(name: string, email?: string, phoneNumber?: string): Observable<SignupResult> {
+        if (name == SignupServiceStub.validName && (email == SignupServiceStub.validEmail || phoneNumber == SignupServiceStub.validPhoneNumber)) {
+            return Observable.of(new SignupResult(true, undefined));
+        } else if (name == SignupServiceStub.inUseName && email == SignupServiceStub.inUseEmail) {
+            return Observable.of(new SignupResult(false, [
+                { 'email': 'The specified e-mail is in use' }
+            ]));
+        } else if (name == SignupServiceStub.inUseName && phoneNumber == SignupServiceStub.inUsePhoneNumber) {
+            return Observable.of(new SignupResult(false, [
+                { 'phoneNumber': 'The specified phone number is in use' }
+            ]));
+        } else if (name == SignupServiceStub.apiFailName && (email == SignupServiceStub.apiFailEmail || phoneNumber == SignupServiceStub.apiFailPhoneNumber)) {
+            return Observable.throw("Internal server error");
+        } else {
+            return Observable.throw("Unknown details used with stub");
+        }
+    }
+
+}
---
+++ frontend/src/app/signup/index.ts
@@ -0,0 +1,5 @@
+export * from './signup-base.component';
+export * from './signup-alt.component';
+export * from './signup.component';
+export * from './signup.service';
+export * from './signup.routes';
