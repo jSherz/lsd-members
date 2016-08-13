dd063ec6c8aa8f4afa00cc011e0eef23fc8275c7
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-08-13 21:26:49 +0100

    Added specs for alt sign-up component.

--- frontend/src/app/signup/alt/signup-alt.component.spec.ts
+++ frontend/src/app/signup/alt/signup-alt.component.spec.ts
@@ -1,15 +1,83 @@
 /* tslint:disable:no-unused-variable */
 
-import {
-  beforeEach, beforeEachProviders,
-  describe, xdescribe,
-  expect, it, xit,
-  async, inject
-} from '@angular/core/testing';
-import { SignupAltComponent } from './signup-alt.component';
+import { FormBuilder } from '@angular/forms';
+import { Router      } from '@angular/router';
 
-beforeEachProviders(() => [SignupAltComponent]);
+import { SignupAltComponent } from './signup-alt.component';
+import { SignupServiceStub  } from '../service/signup.service.stub';
+import { SignupService      } from '../service/signup.service';
+
+class TestSetup {
+  router: Router;
+  service: SignupService;
+  component: SignupAltComponent;
+
+  constructor(router: Router, service: SignupService, component: SignupAltComponent) {
+    this.router = router;
+    this.service = service;
+    this.component = component;
+  }
+}
+
+function mockComp(): TestSetup {
+  let keys = [];
+  for (let key in Router.prototype) {
+    keys.push(key);
+  }
+
+  let builder = new FormBuilder();
+  let router = jasmine.createSpyObj("MockRouter", keys);
+  let service = new SignupServiceStub();
+
+  return new TestSetup(router, service, new SignupAltComponent(builder, router, service));
+}
 
 describe('Signup Alt Component', () => {
+
+  it('shows a generic error message if the API call fails', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.apiFailName, email: SignupServiceStub.apiFailEmail });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    expect(test.router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('clears the generic error message if a successful API call is made at a later time', () => {
+    let test = mockComp();
+
+    // Ensure error showing
+    test.component.signup({ name: SignupServiceStub.apiFailName, email: SignupServiceStub.apiFailEmail });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    // API call - successful response
+    test.component.signup({ name: SignupServiceStub.validName, email: SignupServiceStub.validEmail });
+    expect(test.component.apiRequestFailed).toEqual(false);
+
+    // Ensure error showing
+    test.component.signup({ name: SignupServiceStub.apiFailName, email: SignupServiceStub.apiFailEmail });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    // API call - returned errors
+    test.component.signup({ name: SignupServiceStub.inUseName, email: SignupServiceStub.inUseEmail });
+    expect(test.component.apiRequestFailed).toEqual(false);
+  });
+
+  it('navigates to the thank-you page if signing up succeeds', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.validName, email: SignupServiceStub.validEmail });
+
+    expect(test.router.navigate).toHaveBeenCalled();
+  });
+
+  it('shows errors against the correct field(s) with a successful API call but errors returned', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.inUseName, email: SignupServiceStub.inUseEmail });
+    expect(test.component.errors[0].email).toEqual("The specified e-mail is in use");
+
+    expect(test.router.navigate).not.toHaveBeenCalled();
+  });
 
 });
--- frontend/src/app/signup/service/signup.service.stub.ts
+++ frontend/src/app/signup/service/signup.service.stub.ts
@@ -1,38 +1,46 @@
-import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { ISignupService, SignupResult } from '../index';
+import { Observable                  } from 'rxjs/Observable';
+import { SignupService, SignupResult } from '../index';
 
-export class SignupServiceStub implements ISignupService {
+export class SignupServiceStub extends SignupService {
 
-    static validName: string = "Abby North";
-    static validEmail: string = "AbbyNorth@teleworm.us";
-    static validPhoneNumber: string = "07058074719";
+  static validName: string = "Abby North";
+  static validEmail: string = "AbbyNorth@teleworm.us";
+  static validPhoneNumber: string = "07058074719";
 
-    static inUseName: string = "Mohammed Charlton";
-    static inUseEmail: string = "MohammedCharlton@jourrapide.com";
-    static inUsePhoneNumber: string = "07985457231";
+  static inUseName: string = "Mohammed Charlton";
+  static inUseEmail: string = "MohammedCharlton@jourrapide.com";
+  static inUsePhoneNumber: string = "07985457231";
 
-    static apiFailName: string = "Amy Farrell";
-    static apiFailEmail: string = "AmyFarrell@armyspy.com";
-    static apiFailPhoneNumber: string = "07827651140";
+  static apiFailName: string = "Amy Farrell";
+  static apiFailEmail: string = "AmyFarrell@armyspy.com";
+  static apiFailPhoneNumber: string = "07827651140";
 
-    signup(name: string, email?: string, phoneNumber?: string): Observable<SignupResult> {
-        if (name == SignupServiceStub.validName && (email == SignupServiceStub.validEmail || phoneNumber == SignupServiceStub.validPhoneNumber)) {
-            return Observable.of(new SignupResult(true, undefined));
-        } else if (name == SignupServiceStub.inUseName && email == SignupServiceStub.inUseEmail) {
-            return Observable.of(new SignupResult(false, [
-                { 'email': 'The specified e-mail is in use' }
-            ]));
-        } else if (name == SignupServiceStub.inUseName && phoneNumber == SignupServiceStub.inUsePhoneNumber) {
-            return Observable.of(new SignupResult(false, [
-                { 'phoneNumber': 'The specified phone number is in use' }
-            ]));
-        } else if (name == SignupServiceStub.apiFailName && (email == SignupServiceStub.apiFailEmail || phoneNumber == SignupServiceStub.apiFailPhoneNumber)) {
-            return Observable.throw("Internal server error");
-        } else {
-            return Observable.throw("Unknown details used with stub");
-        }
+  signup(name: string, phoneNumber: string): Observable<SignupResult> {
+    if (name == SignupServiceStub.validName && phoneNumber == SignupServiceStub.validPhoneNumber) {
+      return Observable.of(new SignupResult(true, {}));
+    } else if (name == SignupServiceStub.inUseName && phoneNumber == SignupServiceStub.inUsePhoneNumber) {
+      return Observable.of(new SignupResult(false, [
+        { 'phoneNumber': 'The specified phone number is in use' }
+      ]));
+    } else if (name == SignupServiceStub.apiFailName && phoneNumber == SignupServiceStub.apiFailPhoneNumber) {
+      return Observable.throw("Internal server error");
+    } else {
+      return Observable.throw("Unknown details used with stub");
     }
+  }
+
+  signupAlt(name: string, email: string): Observable<SignupResult> {
+    if (name == SignupServiceStub.validName && email == SignupServiceStub.validEmail) {
+      return Observable.of(new SignupResult(true, {}));
+    } else if (name == SignupServiceStub.inUseName && email == SignupServiceStub.inUseEmail) {
+      return Observable.of(new SignupResult(false, [
+        { 'email': 'The specified e-mail is in use' }
+      ]));
+    } else if (name == SignupServiceStub.apiFailName && email == SignupServiceStub.apiFailEmail) {
+      return Observable.throw("Internal server error");
+    } else {
+      return Observable.throw("Unknown details used with stub");
+    }
+  }
 
 }
