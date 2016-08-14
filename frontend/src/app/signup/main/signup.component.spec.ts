75b1ddc41fb9e0754b4a0256726b51321f37105c
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-08-14 09:37:27 +0100

    Wired up main sign-up form.

--- frontend/src/app/signup/main/signup.component.spec.ts
+++ frontend/src/app/signup/main/signup.component.spec.ts
@@ -7,6 +7,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { APP_ROUTER_PROVIDERS   } from '../../app.routes';
 import { SignupComponent        } from './signup.component';
+import {SignupService} from "../../../../signup.service";
+import {SignupServiceStub} from "../service/signup.service.stub";
 
 beforeEach(() => addProviders([
   APP_ROUTER_PROVIDERS,
@@ -17,8 +19,85 @@ beforeEach(() => addProviders([
   SignupComponent
 ]));
 
-describe('LSD.SignupComponent', () => {
-  it('should create the app', inject([SignupComponent], (app: SignupComponent) => {
-    expect(app).toBeTruthy();
-  }));
+describe('SignupComponent', () => {
+
+  it('shows a generic error message if the API call fails', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.apiFailName, phoneNumber: SignupServiceStub.apiFailPhoneNumber });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    expect(test.router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('clears the generic error message if a successful API call is made at a later time', () => {
+    let test = mockComp();
+
+    // Ensure error showing
+    test.component.signup({ name: SignupServiceStub.apiFailName, phoneNumber: SignupServiceStub.apiFailPhoneNumber });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    // API call - successful response
+    test.component.signup({ name: SignupServiceStub.validName, phoneNumber: SignupServiceStub.validPhoneNumber });
+    expect(test.component.apiRequestFailed).toEqual(false);
+
+    // Ensure error showing
+    test.component.signup({ name: SignupServiceStub.apiFailName, phoneNumber: SignupServiceStub.apiFailPhoneNumber });
+    expect(test.component.apiRequestFailed).toEqual(true);
+
+    // API call - returned errors
+    test.component.signup({ name: SignupServiceStub.inUseName, phoneNumber: SignupServiceStub.inUsePhoneNumber });
+    expect(test.component.apiRequestFailed).toEqual(false);
+  });
+
+  it('navigates to the thank-you page if signing up succeeds', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.validName, phoneNumber: SignupServiceStub.validPhoneNumber });
+
+    expect(test.router.navigate).toHaveBeenCalled();
+  });
+
+  it('shows errors against the correct field(s) with a successful API call but errors returned', () => {
+    let test = mockComp();
+
+    test.component.signup({ name: SignupServiceStub.inUseName, phoneNumber: SignupServiceStub.inUsePhoneNumber });
+    expect(test.component.errors[0].phoneNumber).toEqual("The specified phone number is in use");
+
+    expect(test.router.navigate).not.toHaveBeenCalled();
+  });
+
 });
+
+/**
+ * A holder class for test fixtures.
+ */
+class TestSetup {
+  router: Router;
+  service: SignupService;
+  component: SignupComponent;
+
+  constructor(router: Router, service: SignupService, component: SignupComponent) {
+    this.router = router;
+    this.service = service;
+    this.component = component;
+  }
+}
+
+/**
+ * Create a new component with mocked router, stubbed service and a real form builder.
+ *
+ * @returns {TestSetup}
+ */
+function mockComp(): TestSetup {
+  let keys = [];
+  for (let key in Router.prototype) {
+    keys.push(key);
+  }
+
+  let builder = new FormBuilder();
+  let router = jasmine.createSpyObj('MockRouter', keys);
+  let service = new SignupServiceStub();
+
+  return new TestSetup(router, service, new SignupComponent(builder, router, service));
+}
--- frontend/src/app/signup/main/signup.component.ts
+++ frontend/src/app/signup/main/signup.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component                 } from '@angular/core';
+import { Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { HTTP_PROVIDERS            } from '@angular/http';
 import {
   FormControl,
   FormBuilder,
@@ -7,12 +8,21 @@ import {
   Validators,
   REACTIVE_FORM_DIRECTIVES
 } from '@angular/forms';
+
 import { CustomValidators } from '../../utils';
+import {
+  SignupService,
+  SignupServiceImpl
+} from '../service/signup.service';
 
 @Component({
   selector: 'signup-component',
   templateUrl: 'signup.component.html',
-  directives: [REACTIVE_FORM_DIRECTIVES]
+  directives: [REACTIVE_FORM_DIRECTIVES],
+  providers: [
+    HTTP_PROVIDERS,
+    { provide: SignupService, useClass: SignupServiceImpl }
+  ]
 })
 export class SignupComponent {
 
@@ -21,7 +31,35 @@ export class SignupComponent {
   ctrlName: FormControl;
   ctrlPhoneNumber: FormControl;
 
-  constructor(private builder: FormBuilder, private router: Router) {
+  /**
+   * Set when an API request fails.
+   *
+   * @type {boolean}
+   */
+  apiRequestFailed: boolean = false;
+
+  /**
+   * Should we show the loading animation?
+   *
+   * @type {boolean}
+   */
+  showThrobber: boolean = false;
+
+  /**
+   * Any errors returned by the API.
+   */
+  errors: {
+    name: undefined,
+    email: undefined
+  };
+
+  /**
+   * Build the sign-up form, including setting up any validation.
+   *
+   * @param builder
+   * @param router
+   */
+  constructor(private builder: FormBuilder, private router: Router, private signupService: SignupService) {
     this.ctrlName = new FormControl('', Validators.required);
     this.ctrlPhoneNumber = new FormControl('', Validators.compose([Validators.required, CustomValidators.phoneNumber]));
 
@@ -31,8 +69,50 @@ export class SignupComponent {
     });
   }
 
-  signup() {
-    this.router.navigate(['sign-up', 'thank-you']);
+  /**
+   * Attempt to sign-up a user by name and phone number.
+   *
+   * Will show an error message if the API call fails or if it returns an error.
+   *
+   * Navigates to the "thank-you" page if signing up succeeds.
+   *
+   * @param user (with params "name" and "phoneNumber")
+   */
+  signup(user) {
+    this.showThrobber = true;
+
+    this.signupService.signup(user.name, user.phoneNumber).subscribe(
+      result => {
+        // API request succeeded, check result
+        this.apiRequestFailed = false;
+        this.showThrobber = false;
+
+        if (result.success) {
+          this.router.navigate(['sign-up', 'thank-you']);
+        } else {
+          this.errors = result.errors;
+        }
+      },
+      error => {
+        // API request failed, show generic error
+        console.log('Sign-up alt failed: ' + error);
+
+        this.apiRequestFailed = true;
+        this.showThrobber = false;
+      }
+    );
+  }
+
+  /**
+   * Translates an error message key received from the server.
+   *
+   * @param key
+   * @returns {any}
+   */
+  translate(key: string) {
+    return {
+      'error.inUse': 'This phone number is in use. Have you already signed up?'
+    }[key];
   }
 
 }
