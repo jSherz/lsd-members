d747c145c907b30c1c35ae7f49595ab22378a5df
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-08-21 11:49:48 +0100

    Wired up service. Need to refactor out common service methods.

--- frontend/src/app/admin/member-search/member-search.service.ts
+++ frontend/src/app/admin/member-search/member-search.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Headers, RequestOptions, Response} from "@angular/http";
 import {Observable} from "rxjs";
+import {ErrorObservable} from "rxjs/observable/ErrorObservable";
 
 export class SearchResult {
 
+  uuid: string;
   name: string;
   phoneNumber: string;
   email: string;
 
-  constructor(name, phoneNumber, email) {
+  constructor(uuid, name, phoneNumber, email) {
+    this.uuid = uuid;
     this.name = name;
     this.phoneNumber = phoneNumber;
     this.email = email;
@@ -25,18 +28,62 @@ export abstract class MemberSearchService {
 @Injectable()
 export class MemberSearchServiceImpl extends MemberSearchService {
 
+  private memberSearchUrl = 'http://localhost:8080/api/v1/members/search';
+
   constructor(private http: Http) {
     super();
   }
 
   search(term: string): Observable<SearchResult[]> {
-    return Observable.of([
-      new SearchResult('Sarah Sheppard', '+447763098707', 'SarahSheppard@dayrep.com'),
-      new SearchResult('Finlay Bennett', '+447835492846', 'LillyKemp@rhyta.com'),
-      new SearchResult('William Stanley', '+447945258738', 'TomIqbal@jourrapide.com'),
-      new SearchResult('Jack Goodwin', '+447715587255', 'AbbieMurphy@teleworm.us'),
-      new SearchResult('Lilly Kemp', '+447887869500', 'LiamDixon@jourrapide.com')
-    ]);
+    let body = {
+      searchTerm: term
+    };
+
+    return this.postAsJson(this.memberSearchUrl, body)
+      .map(this.extractJson)
+      .catch(this.handleError);
+  }
+
+  /**
+   * Extract the JSON body of a response.
+   *
+   * @param res
+   * @returns
+   */
+  private extractJson(res: Response): SearchResult[] {
+    let body = res.json();
+    return body || [];
+  }
+
+  /**
+   * Handle a generic error encountered when performing an AJAX request.
+   *
+   * @param err
+   * @param caught
+   * @returns {ErrorObservable}
+   */
+  private handleError<R, T>(err: any, caught: Observable<T>): ErrorObservable {
+    let errMsg = (err.message) ? err.message : err.status ? `${err.status} - ${err.statusText}` : 'Server error';
+    console.error(errMsg);
+
+    return Observable.throw(new Error(errMsg));
+  }
+
+  /**
+   * Build a post request to the given URL with the given data (serialized as JSON).
+   *
+   * The request is sent with a content type of 'application/json'.
+   *
+   * @param url
+   * @param data
+   * @returns {Observable<Response>}
+   */
+  private postAsJson(url: string, data: any) {
+    let body = JSON.stringify(data);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(url, body, options);
   }
 
 }
