e5376fdb05230c415f48b89b91ab6ac6146af9ac
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-08-28 10:34:15 +0100

    Minor style improvements.

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  templateUrl: 'contact.component.html'
+  templateUrl: 'contact.component.html',
+  styleUrls: ['contact.component.sass']
 })
 export class ContactComponent implements OnInit {
 
