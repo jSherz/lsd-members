914f7dad49de84e274aee90c94ecd04702dac144
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-10-21 19:13:12 +0100

    Got mass text component spec compiling.

--- frontend/src/app/admin/mass-text/mass-text.component.spec.ts
+++ frontend/src/app/admin/mass-text/mass-text.component.spec.ts
@@ -1,11 +1,34 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { MassTextComponent } from './mass-text.component';
+import {TestBed, async} from '@angular/core/testing';
+import {FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
+
+import {MassTextComponent} from './mass-text.component';
+import {StubMassTextService} from './mass-text.service.stub';
+
 
 describe('Component: MassText', () => {
-  it('should create an instance', () => {
-    let component = new MassTextComponent();
+
+  function mockComp(): MassTextComponent {
+    let keys = [];
+    for (let key in Router.prototype) {
+      if (Router.prototype.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
+
+    let builder = new FormBuilder();
+    let router = jasmine.createSpyObj('MockRouter', keys);
+    let service = new StubMassTextService();
+
+    return new MassTextComponent(builder, router, service);
+  }
+
+  it('should create an instance', async(() => {
+    let component = mockComp();
+
     expect(component).toBeTruthy();
-  });
+  }));
+
 });
---
+++ frontend/src/app/admin/mass-text/mass-text.service.stub.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import * as moment from 'moment';
+import {Observable} from 'rxjs';
+
+import {MassTextService, MassTextSendResponse} from './mass-text.service';
+
+
+@Injectable()
+export class StubMassTextService extends MassTextService {
+
+  constructor() {
+    super(undefined, undefined);
+  }
+
+  send(startDate: moment.Moment, endDate: moment.Moment, template: string,
+       expectedRendered: string): Observable<MassTextSendResponse> {
+    return Observable.of(new MassTextSendResponse(true, undefined, '12345'));
+  }
+
+}
