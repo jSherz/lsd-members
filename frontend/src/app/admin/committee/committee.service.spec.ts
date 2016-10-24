d47c7c5071c25598cde1c4cb232d4ccb19cf78f6
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2016-10-24 18:12:14 +0100

    Added basic spec for committee active.

--- frontend/src/app/admin/committee/committee.service.spec.ts
+++ frontend/src/app/admin/committee/committee.service.spec.ts
@@ -1,7 +1,8 @@
 /* tslint:disable:no-unused-variable */
 
 import {inject, async, TestBed} from '@angular/core/testing';
-import {HttpModule} from '@angular/http';
+import {Http, Response, ResponseOptions} from '@angular/http';
+import {Observable} from 'rxjs';
 
 import {CommitteeService, CommitteeServiceImpl} from './committee.service';
 import {ApiKeyService} from '../utils/api-key.service';
@@ -12,18 +13,43 @@ describe('Service: Committee', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpModule
-      ],
       providers: [
-        {provide: ApiKeyService, useValue: new StubApiKeyService('12345')},
+        {
+          provide: Http, useValue: {
+          get(url) {
+            return Observable.of(new Response(new ResponseOptions({
+              body: '[{"uuid": "ade89632-2ccd-4b9f-93f1-44f5adf9704c", "name": "Ann Bass"},' +
+              '{"uuid": "329ad322-fe4a-4bdc-9c5b-030431766e36", "name": "Anna Morales"},' +
+              '{"uuid": "a3fb1c86-af9c-49db-a0ac-3bff22b9352d", "name": "Anthony Smith"}]'
+            })));
+          }
+        }
+        },
+        {provide: ApiKeyService, useValue: new StubApiKeyService('487a2930-8e6a-41a5-bcc0-b7fd7f2421e4')},
         {provide: CommitteeService, useClass: CommitteeServiceImpl}
       ]
     });
   });
 
-  it('should ...', async(inject([CommitteeService], (service: CommitteeService) => {
-    expect(service).toBeTruthy();
+  it('should return the correct active committee', async(inject([CommitteeService], (service: CommitteeService) => {
+    service.active().subscribe(result => {
+      expect(result).toEqual(
+        [
+          {
+            'uuid': 'ade89632-2ccd-4b9f-93f1-44f5adf9704c',
+            'name': 'Ann Bass'
+          },
+          {
+            'uuid': '329ad322-fe4a-4bdc-9c5b-030431766e36',
+            'name': 'Anna Morales'
+          },
+          {
+            'uuid': 'a3fb1c86-af9c-49db-a0ac-3bff22b9352d',
+            'name': 'Anthony Smith'
+          }
+        ]
+      );
+    });
   })));
 
 });
