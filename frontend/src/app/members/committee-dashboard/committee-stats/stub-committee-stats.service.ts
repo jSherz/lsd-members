0d8db347e191781704bd0a332c898bc99f04060f
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-09 16:35:02 +0100

    Resolved lint error.

--- frontend/src/app/members/committee-dashboard/committee-stats/stub-committee-stats.service.ts
+++ frontend/src/app/members/committee-dashboard/committee-stats/stub-committee-stats.service.ts
@@ -6,6 +6,10 @@ export class StubCommitteeStatsService extends CommitteeStatsService {
 
   numReceived: Subject<number> = new Subject<number>();
 
+  constructor() {
+    super(null, null, null);
+  }
+
   getNumReceivedMessages(): Observable<number> {
     return this.numReceived;
   }
