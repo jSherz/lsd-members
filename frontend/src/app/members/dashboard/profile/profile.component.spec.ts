db257e3b9566ea29c0e916db1d68b8046d58057f
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-10 20:46:09 +0100

    Fixed broken spec.

--- frontend/src/app/members/dashboard/profile/profile.component.spec.ts
+++ frontend/src/app/members/dashboard/profile/profile.component.spec.ts
@@ -20,15 +20,13 @@ describe('ProfileComponent', () => {
   });
 
   it('should display a name if one is provided', () => {
-    component.basicInfo = new BasicInfo();
-    component.basicInfo.firstName = 'Billy';
+    component.basicInfo = new BasicInfo('b9424a23-fc0e-4b02-9897-21ba09868c55', 'Billy', null, null);
 
     expect(component.hasName()).toBeTruthy();
   });
 
   it('should display no name if one isn\'t provided', () => {
-    component.basicInfo = new BasicInfo();
-    component.basicInfo.firstName = '';
+    component.basicInfo = new BasicInfo('63f4df43-5da8-4a36-8d7b-5ff984c3d7ce', '', null, null);
     expect(component.hasName()).toBeFalsy();
 
     component.basicInfo = null;
