129ecaf56e3e70d1f05d3586752feac78266f9c0
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-07-22 13:48:40 +0100

    Added missing spec for social login service.

--- frontend/src/app/members/social-login/social-login.service.spec.ts
+++ frontend/src/app/members/social-login/social-login.service.spec.ts
@@ -3,20 +3,45 @@
 import {TestBed, inject, async} from '@angular/core/testing';
 
 import {SocialLoginService, SocialLoginServiceImpl} from './social-login.service';
-import {Http} from '@angular/http';
+import {Http, Response, ResponseOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import {SocialLoginRequest, SocialLoginResponse} from './model';
 
 describe('SocialLoginService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        {provide: SocialLoginService, useClass: SocialLoginServiceImpl},
-        {
-          provide: Http, useValue: {}
-        }
-      ]
+
+  it('makes a login request and parses the response', () => {
+    const http: any = {
+      post: () => {
+        return Observable.of(new Response(new ResponseOptions({
+          status: 200,
+          body: JSON.stringify(new SocialLoginResponse(true, null, 'jwt.1.2', false)),
+          url: 'https://myspace.com'
+        })));
+      }
+    };
+    const postSpy = spyOn(http, 'post').and.callThrough();
+
+    const service: SocialLoginService = new SocialLoginServiceImpl(http);
+
+    expect(postSpy).not.toHaveBeenCalled();
+
+    const response = service.login('235y8werin285wefsdf23');
+    response.subscribe((result) => {
+      expect(result.success).toBeTruthy();
+      expect(result.error).toBeNull();
+      expect(result.jwt).toEqual('jwt.1.2');
+      expect(result.committeeMember).toBeFalsy();
     });
+
+    expect(postSpy.calls.count()).toEqual(1);
+
+    const call = postSpy.calls.first();
+    const url = call.args[0];
+    const body = call.args[1];
+
+    expect(url).toEqual('https://api.dev.leedsskydivers.com/api/v1/social-login/verify');
+    expect(body).toEqual(new SocialLoginRequest('235y8werin285wefsdf23'));
   });
 
 });
