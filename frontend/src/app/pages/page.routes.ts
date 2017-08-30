3eb75cea621905bd077a69094798d391e9a2db9a
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2017-08-30 20:17:01 +0100

    Removec circular reference.

--- frontend/src/app/pages/page.routes.ts
+++ frontend/src/app/pages/page.routes.ts
@@ -1,26 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import {
-  BaseComponent,
-  HomeComponent,
-  AboutComponent,
-  ContactComponent,
-  CommitteeComponent,
-  FaqComponent,
-  PricesComponent
-} from './index';
+import {AboutComponent} from './about';
+import {BaseComponent} from './base';
+import {CommitteeComponent} from './committee';
+import {ContactComponent} from './contact';
+import {FaqComponent} from './faq';
+import {HomeComponent} from './home';
+import {PricesComponent} from './prices';
 
 export const PageRoutes: Routes = [
   {
     path: '',
     component: BaseComponent,
     children: [
-      { path: '', component: HomeComponent },
-      { path: 'about-the-club', component: AboutComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: 'committee', component: CommitteeComponent },
-      { path: 'faq', component: FaqComponent },
-      { path: 'prices', component: PricesComponent }
+      {path: '', component: HomeComponent},
+      {path: 'about-the-club', component: AboutComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'committee', component: CommitteeComponent},
+      {path: 'faq', component: FaqComponent},
+      {path: 'prices', component: PricesComponent}
     ]
   }
 ];
