d8c9c2dce533d3c20eba43ab692081a3a57180f9
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2018-08-30 21:37:04 +0100

    Updated copyright year.

--- backend/src/main/scala/com/jsherz/luskydive/core/FBSignedRequest.java
+++ backend/src/main/scala/com/jsherz/luskydive/core/FBSignedRequest.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  * <p>
- * Copyright (c) 2016 James Sherwood-Jones <james.sherwoodjones@gmail.com>
+ * Copyright (c) 2016-2018 James Sherwood-Jones <james.sherwoodjones@gmail.com>
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
